756dffeaadf9acefbdc270b8460a292ece2dc8b9
Sukuna <atomicdomb@gmail.com>
2024-11-27 11:01:40 +0200

    Added cardware_btc files

---
+++ pkg/cardware_btc.js
@@ -0,0 +1,4 @@
+import * as wasm from "./cardware_btc_bg.wasm";
+import { __wbg_set_wasm } from "./cardware_btc_bg.js";
+__wbg_set_wasm(wasm);
+export * from "./cardware_btc_bg.js";
---
+++ pkg/cardware_btc_bg.wasm.d.ts
@@ -0,0 +1,28 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_wallet_free(a: number): void;
+export function wallet_new(a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number): number;
+export function wallet_sync(a: number): number;
+export function wallet_sync_to_depth(a: number, b: number, c: number): number;
+export function wallet_broadcast(a: number, b: number, c: number): number;
+export function wallet_send(a: number, b: number, c: number, d: number, e: number, f: number, g: number): void;
+export function wallet_estimate_fee(a: number, b: number, c: number, d: number, e: number, f: number): number;
+export function wallet_set_trusted_pending(a: number, b: number, c: number): void;
+export function wallet_get_trusted_pending(a: number, b: number): void;
+export function wallet_balance(a: number): number;
+export function wallet_unconfirmed_balance(a: number): number;
+export function wallet_address(a: number, b: number): void;
+export function wallet_new_address(a: number, b: number, c: number, d: number): void;
+export function rustsecp256k1_v0_10_0_context_create(a: number): number;
+export function rustsecp256k1_v0_10_0_context_destroy(a: number): void;
+export function rustsecp256k1_v0_10_0_default_illegal_callback_fn(a: number, b: number): void;
+export function rustsecp256k1_v0_10_0_default_error_callback_fn(a: number, b: number): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function wasm_bindgen__convert__closures__invoke1_mut__h377e19fc35db30a7(a: number, b: number, c: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function wasm_bindgen__convert__closures__invoke2_mut__h3b961219945d4f1f(a: number, b: number, c: number, d: number): void;
