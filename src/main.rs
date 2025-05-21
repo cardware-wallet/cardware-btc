a4310a929d67e89b9b8bb3a4e6296890b9550e00
maans-dev <hprademeyer123@gmail.com>
2025-05-21 12:20:02 +0200

    fix: remove unused imports in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
 use cardware_btc::Wallet;
-use cardware_btc::array_to_hex;
-use cardware_btc::convert_psbt_to_qr;
-use std::fs;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
